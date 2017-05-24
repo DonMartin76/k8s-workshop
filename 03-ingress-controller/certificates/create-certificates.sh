a2f1e1afbf4ca2f039dc4d3972ef7383ef56f9cd
SSpeights <scott.speights@haufe-lexware.com>
2017-05-24 10:13:57 +0200

    Check for microsoft and use escaped creds to create cert

--- 03-ingress-controller/certificates/create-certificates.sh
+++ 03-ingress-controller/certificates/create-certificates.sh
@@ -8,8 +8,14 @@ fi
 source ./env.sh
 
 mkdir -p certificates
+$(uname -o)
 
+if [ $(uname -o) = "Msys" ]; then
+openssl req -x509 -newkey rsa:2048 -keyout certificates/portal-key.pem -out certificates/portal-cert.pem -nodes -subj "_//CN=${PORTAL_NETWORK_PORTALHOST}" -days 365
+openssl req -x509 -newkey rsa:2048 -keyout certificates/api-key.pem -out certificates/api-cert.pem -nodes -subj "//CN=${PORTAL_NETWORK_APIHOST}" -days 365
+openssl req -x509 -newkey rsa:2048 -keyout certificates/notes-key.pem -out certificates/notes-cert.pem -nodes -subj "//CN=${APP_HOST}" -days 365
+else then
 openssl req -x509 -newkey rsa:2048 -keyout certificates/portal-key.pem -out certificates/portal-cert.pem -nodes -subj "/CN=${PORTAL_NETWORK_PORTALHOST}" -days 365
 openssl req -x509 -newkey rsa:2048 -keyout certificates/api-key.pem -out certificates/api-cert.pem -nodes -subj "/CN=${PORTAL_NETWORK_APIHOST}" -days 365
 openssl req -x509 -newkey rsa:2048 -keyout certificates/notes-key.pem -out certificates/notes-cert.pem -nodes -subj "/CN=${APP_HOST}" -days 365
-
+fi
