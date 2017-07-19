d29d9fa22e8876151c98bbeac7dddafa33cbf2f7
Martin Danielsson <martin@danielsson.de>
2017-07-19 11:52:36 +0200

    More Windows fixes.

--- 03-ingress-controller/certificates/create-certificates.sh
+++ 03-ingress-controller/certificates/create-certificates.sh
@@ -8,18 +8,24 @@ fi
 source ./env.sh
 
 mkdir -p certificates
-$(uname -o)
+uname -o
 # Check to see if the operating system is Windows and use Windows escaping
 
 for d in portal api notes; do
   echo $d
-  openssl genrsa -out $d-key.pem 2048
+  openssl genrsa -out certificates/$d-key.pem 2048
   commonName=${APP_HOST}
   if [[ $d == portal ]]; then commonName="${PORTAL_NETWORK_PORTALHOST}"; fi
   if [[ $d == gateway ]]; then commonName="${PORTAL_NETWORK_APIHOST}"; fi
-  openssl req -new -sha256 -key $d-key.pem -out $d.csr -subj "/C=DE/ST=Baden-Wuerttemberg/L=Freiburg/CN=${commonName}"
-  openssl req -x509 -sha256 -days 730 -key $d-key.pem -in $d.csr -out $d-cert.pem
-  openssl req -in $d.csr -text -noout | grep -i "Signature.*SHA256" && echo "All is well" || echo "This certificate will stop working in 2017! You must update OpenSSL to generate a widely-compatible certificate"
+  if [[ $(uname -o) == Msys ]]; then
+    # This is a mindblowingly stupid syntax, see
+    # https://stackoverflow.com/questions/31506158/running-openssl-from-a-bash-script-on-windows-subject-does-not-start-with
+    openssl req -new -sha256 -key certificates/$d-key.pem -out certificates/$d.csr -subj "//C=DE\ST=Baden-Wuerttemberg\L=Freiburg\CN=${commonName}"
+  else
+    openssl req -new -sha256 -key certificates/$d-key.pem -out certificates/$d.csr -subj "/C=DE/ST=Baden-Wuerttemberg/L=Freiburg/CN=${commonName}"
+  fi
+  openssl req -x509 -sha256 -days 730 -key certificates/$d-key.pem -in certificates/$d.csr -out certificates/$d-cert.pem
+  openssl req -in certificates/$d.csr -text -noout | grep -i "Signature.*SHA256" && echo "All is well" || echo "This certificate will stop working in 2017! You must update OpenSSL to generate a widely-compatible certificate"
 done
 
 
