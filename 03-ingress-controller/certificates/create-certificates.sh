e3115c661ce08187e1113edf8b5f71327627d848
Martin Danielsson <martin@danielsson.de>
2017-07-19 11:53:31 +0200

    Clean up

--- 03-ingress-controller/certificates/create-certificates.sh
+++ 03-ingress-controller/certificates/create-certificates.sh
@@ -27,14 +27,3 @@ for d in portal api notes; do
   openssl req -x509 -sha256 -days 730 -key certificates/$d-key.pem -in certificates/$d.csr -out certificates/$d-cert.pem
   openssl req -in certificates/$d.csr -text -noout | grep -i "Signature.*SHA256" && echo "All is well" || echo "This certificate will stop working in 2017! You must update OpenSSL to generate a widely-compatible certificate"
 done
-
-
-# if [ "$(uname -o)" = "Msys" ]; then
-#     openssl req -x509 -newkey rsa:2048 -keyout certificates/portal-key.pem -out certificates/portal-cert.pem -nodes -subj "//CN=${PORTAL_NETWORK_PORTALHOST},//subjectAltName=${PORTAL_NETWORK_PORTALHOST}" -days 365
-#     openssl req -x509 -newkey rsa:2048 -keyout certificates/api-key.pem -out certificates/api-cert.pem -nodes -subj "//CN=${PORTAL_NETWORK_APIHOST},//subjectAltName=${PORTAL_NETWORK_APIHOST}" -days 365
-#     openssl req -x509 -newkey rsa:2048 -keyout certificates/notes-key.pem -out certificates/notes-cert.pem -nodes -subj "//CN=${APP_HOST},//subjectAltName=${APP_HOST}" -days 365
-# else
-#     openssl req -x509 -newkey rsa:2048 -keyout certificates/portal-key.pem -out certificates/portal-cert.pem -nodes -subj "/CN=${PORTAL_NETWORK_PORTALHOST},/subjectAltName=${PORTAL_NETWORK_PORTALHOST}" -days 365
-#     openssl req -x509 -newkey rsa:2048 -keyout certificates/api-key.pem -out certificates/api-cert.pem -nodes -subj "/CN=${PORTAL_NETWORK_APIHOST},/subjectAltName=${PORTAL_NETWORK_APIHOST}" -days 365
-#     openssl req -x509 -newkey rsa:2048 -keyout certificates/notes-key.pem -out certificates/notes-cert.pem -nodes -subj "/CN=${APP_HOST},/subjectAltName=${APP_HOST}" -days 365
-# fi
