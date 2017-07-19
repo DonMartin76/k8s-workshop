21ddb31182036c5ee678e8fa3c0983497d908397
Martin Danielsson <martin@danielsson.de>
2017-07-19 08:39:17 +0200

    Windows fix?

--- 03-ingress-controller/certificates/create-certificates.sh
+++ 03-ingress-controller/certificates/create-certificates.sh
@@ -11,11 +11,11 @@ mkdir -p certificates
 $(uname -o)
 # Check to see if the operating system is Windows and use Windows escaping
 if [ "$(uname -o)" = "Msys" ]; then
-    openssl req -x509 -newkey rsa:2048 -keyout certificates/portal-key.pem -out certificates/portal-cert.pem -nodes -subj "//CN=${PORTAL_NETWORK_PORTALHOST}" -days 365
-    openssl req -x509 -newkey rsa:2048 -keyout certificates/api-key.pem -out certificates/api-cert.pem -nodes -subj "//CN=${PORTAL_NETWORK_APIHOST}" -days 365
-    openssl req -x509 -newkey rsa:2048 -keyout certificates/notes-key.pem -out certificates/notes-cert.pem -nodes -subj "//CN=${APP_HOST}" -days 365
+    openssl req -x509 -newkey rsa:2048 -keyout certificates/portal-key.pem -out certificates/portal-cert.pem -nodes -subj "//CN=${PORTAL_NETWORK_PORTALHOST},//subjectAltName=${PORTAL_NETWORK_PORTALHOST}" -days 365
+    openssl req -x509 -newkey rsa:2048 -keyout certificates/api-key.pem -out certificates/api-cert.pem -nodes -subj "//CN=${PORTAL_NETWORK_APIHOST},//subjectAltName=${PORTAL_NETWORK_APIHOST}" -days 365
+    openssl req -x509 -newkey rsa:2048 -keyout certificates/notes-key.pem -out certificates/notes-cert.pem -nodes -subj "//CN=${APP_HOST},//subjectAltName=${APP_HOST}" -days 365
 else
-    openssl req -x509 -newkey rsa:2048 -keyout certificates/portal-key.pem -out certificates/portal-cert.pem -nodes -subj "/CN=${PORTAL_NETWORK_PORTALHOST}" -days 365
-    openssl req -x509 -newkey rsa:2048 -keyout certificates/api-key.pem -out certificates/api-cert.pem -nodes -subj "/CN=${PORTAL_NETWORK_APIHOST}" -days 365
-    openssl req -x509 -newkey rsa:2048 -keyout certificates/notes-key.pem -out certificates/notes-cert.pem -nodes -subj "/CN=${APP_HOST}" -days 365
+    openssl req -x509 -newkey rsa:2048 -keyout certificates/portal-key.pem -out certificates/portal-cert.pem -nodes -subj "/CN=${PORTAL_NETWORK_PORTALHOST},/subjectAltName=${PORTAL_NETWORK_PORTALHOST}" -days 365
+    openssl req -x509 -newkey rsa:2048 -keyout certificates/api-key.pem -out certificates/api-cert.pem -nodes -subj "/CN=${PORTAL_NETWORK_APIHOST},/subjectAltName=${PORTAL_NETWORK_APIHOST}" -days 365
+    openssl req -x509 -newkey rsa:2048 -keyout certificates/notes-key.pem -out certificates/notes-cert.pem -nodes -subj "/CN=${APP_HOST},/subjectAltName=${APP_HOST}" -days 365
 fi
